1808d5b5754fa28213903a10c3eba907a3bfc40f
kellyRaines05 <kellyrainbowraines@gmail.com>
2025-09-09 01:04:34 -0700

    test

---
+++ test_vlms.py
@@ -0,0 +1,43 @@
+
+from transformers import AutoProcessor, AutoModelForImageTextToText
+import torch
+from PIL import Image
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+model_path = "HuggingFaceTB/SmolVLM2-2.2B-Instruct"
+processor = AutoProcessor.from_pretrained(model_path)
+model = AutoModelForImageTextToText.from_pretrained(model_path,
+                                                torch_dtype=torch.bfloat16,
+                                                _attn_implementation="flash_attention_2" if DEVICE == "cuda" else "eager").to(DEVICE)
+
+# Load image
+image = Image.open("example_image.png")
+
+# Create input messages
+messages = [
+    {
+        "role": "system",
+        "content": [
+            {"type": "text", "text": "You are a master moodboard creator. For any occasion, any theme, any idea, you can mix and match together images and colors that relate to the user request."}
+        ],
+        "role": "user",
+        "content": [
+            {"type": "image"},
+            {"type": "text", "text": " I am trying to make a moodboard for a fashion show. I want it to fit the desert theme: Fall/Winter time. What are some keywords I should look for that are a similar vibe to the reference image?"}
+        ]
+    },
+]
+
+# Prepare inputs
+prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
+inputs = processor(text=prompt, images=[image], return_tensors="pt")
+inputs = inputs
+
+# Generate outputs
+generated_ids = model.generate(**inputs, max_new_tokens=500)
+generated_texts = processor.batch_decode(
+    generated_ids,
+    skip_special_tokens=True,
+)
+
+print(generated_texts[0])
